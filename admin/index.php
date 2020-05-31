7031bc78bc1a18bb0d5433121a6a127af1abe2cd
Md Irshad <32608381+irshadmd@users.noreply.github.com>
2020-05-31 08:29:39 +0530

    issues fixes

--- admin/index.php
+++ admin/index.php
@@ -1,33 +1,30 @@
 <?php
-  session_start();
-  if(isset($_SESSION['admin'])){
-    header('location:home.php');
-  }
+session_start();
+if (isset($_SESSION['admin'])) {
+	header('location:home.php');
+}
 ?>
 <?php include 'includes/header.php'; ?>
+
 <body class="hold-transition login-page">
 	<div class="limiter">
-		<div class="container-login100" style="background-image: url('../images/bg-01.jpg');">
+		<div class="container-login100">
 			<div class="wrap-login100">
 				<form class="login100-form validate-form" form action="login.php" method="POST">
-					<span class="login100-form-logo">
-						<i class="zmdi zmdi-account-add"></i>
-					</span>
-
 					<span class="login100-form-title p-b-34 p-t-27">
-						Admin Log in
+						Admin Login
 					</span>
-          <?php
-        		if(isset($_SESSION['error'])){
-        			echo "
+					<?php
+					if (isset($_SESSION['error'])) {
+						echo "
         				<div class='callout callout-danger text-center mt20'>
-      			  		<p style='color:white' >".$_SESSION['error']."</p>
+      			  		<p style='color:white' >" . $_SESSION['error'] . "</p>
       			  	</div>
         			";
-        			unset($_SESSION['error']);
-        		}
-        	?>
-					<div class="wrap-input100 validate-input" data-validate = "Enter Username">
+						unset($_SESSION['error']);
+					}
+					?>
+					<div class="wrap-input100 validate-input" data-validate="Enter Username">
 						<input class="input100" type="text" name="username" placeholder="Username">
 						<span class="focus-input100" data-placeholder="&#xf207;"></span>
 					</div>
@@ -38,22 +35,14 @@
 					</div>
 
 					<div class="container-login100-form-btn">
-						<button type="submit" name="adminlogin" class="login100-form-btn" ><i class="fa fa-sign-in"></i>   &nbsp Login</button>
-					</div>
-
-					<div class="text-center p-t-90">
-						<a class="txt1" href="#">
-							Forgot Password?
-						</a>
+						<button type="submit" name="adminlogin" class="login100-form-btn"><i class="fa fa-sign-in"></i> &nbsp Login</button>
 					</div>
 				</form>
 			</div>
 		</div>
-      <div class="container-fluid" style="text-align:center">
-        <p>© 2020 MiniBank. All Rights Reserved | Design by MiniBank</p>
-      </div>
-  </div>
-  </div>
-<?php include 'includes/scripts.php' ?>
+	</div>
+	</div>
+	<?php include 'includes/scripts.php' ?>
 </body>
+
 </html>
--- bank/index.php
+++ bank/index.php
@@ -1,13 +1,26 @@
 <?php
-  session_start();
-  if(isset($_SESSION['members'])){
-    header('location:home.php');
-  }
+session_start();
+if (isset($_SESSION['members'])) {
+	header('location:home.php');
+}
 ?>
 <?php include 'includes/header.php'; ?>
+<style>
+	.backhome {
+		margin: 2%;
+	}
+
+</style>
+
 <body class="hold-transition login-page">
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('../images/bg-01.jpg');">
+			<div class="container-fluid" style="text-align:center">
+				<hr>
+				<h3>Welcome to MiniBank!</h3>
+				<h4>Login Here <i class="fa fa-forward"></i></h4>
+				<hr>
+			</div>
 			<div class="wrap-login100">
 				<form class="login100-form validate-form" form action="login.php" method="POST">
 					<span class="login100-form-logo">
@@ -15,19 +28,19 @@
 					</span>
 
 					<span class="login100-form-title p-b-34 p-t-27">
-						Log in
+						Login
 					</span>
-          <?php
-        		if(isset($_SESSION['error'])){
-        			echo "
+					<?php
+					if (isset($_SESSION['error'])) {
+						echo "
         				<div class='callout callout-danger text-center mt20'>
-      			  		<p style='color:white' >".$_SESSION['error']."</p>
+      			  		<p style='color:white' >" . $_SESSION['error'] . "</p>
       			  	</div>
         			";
-        			unset($_SESSION['error']);
-        		}
-        	?>
-					<div class="wrap-input100 validate-input" data-validate = "Enter member">
+						unset($_SESSION['error']);
+					}
+					?>
+					<div class="wrap-input100 validate-input" data-validate="Enter member">
 						<input class="input100" type="text" name="memberid" placeholder="Member ID">
 						<span class="focus-input100" data-placeholder="&#xf207;"></span>
 					</div>
@@ -37,30 +50,20 @@
 						<span class="focus-input100" data-placeholder="&#xf191;"></span>
 					</div>
 
-					<div class="contact100-form-checkbox">
-						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-						<label class="label-checkbox100" for="ckb1">
-							Remember me
-						</label>
-					</div>
-
 					<div class="container-login100-form-btn">
-						<button type="submit" name="login" class="login100-form-btn" ><i class="fa fa-sign-in"></i>   &nbsp Login</button>
+						<button type="submit" name="login" class="login100-form-btn backhome"><i class="fa fa-user"></i> &nbsp Login</button>&nbsp&nbsp
+						<a href="../index.html" class="login100-form-btn backhome"><i class="fa fa-sign-out"></i>&nbsp Back To Website</a>
 					</div>
 
-					<div class="text-center p-t-90">
-						<a class="txt1" href="#">
-							Forgot Password?
-						</a>
-					</div>
 				</form>
 			</div>
 		</div>
-      <div class="container-fluid" style="text-align:center">
-        <p>© 2020 MiniBank. All Rights Reserved | Design by MiniBank</p>
-      </div>
-  </div>
-  </div>
-<?php include 'includes/scripts.php' ?>
+		<div class="container-fluid" style="text-align:center">
+			<p>© 2020 MiniBank. All Rights Reserved</p>
+		</div>
+	</div>
+	</div>
+	<?php include 'includes/scripts.php' ?>
 </body>
+
 </html>
