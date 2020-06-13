f642694fb7c8e04c2ffaaa20e7cd323e715f372c
Md Irshad <32608381+irshadmd@users.noreply.github.com>
2020-06-13 17:19:02 +0530

    growth after 100hrs

--- admin/afterhundredhours.php
+++ admin/afterhundredhours.php
@@ -8,12 +8,31 @@
       $memberid=$row['member_id'];
       $amount=$row['amount'];
       $approvedate=$row['approved_datetime'];
+      $bool=$row['growth'];
 
       if(($datenow>$approvedate) AND ($status=='pendingGet')){
-        $sql = "UPDATE provide_help SET status = 'approved' WHERE id = '$id'";
+        $sql = "UPDATE provide_help SET status = 'approved',growth='true' WHERE id = '$id'";
         $query = $conn->query($sql);
 
         $g_amont = $amount*2;
+
+        $sql = "INSERT INTO growth(member_id,provide_amount,growth_amount) VALUES('$memberid','$amount','$g_amont')";
+        $conn->query($sql);
+
+        $sql = "SELECT * FROM wallet WHERE member_id='$memberid'";
+        $query = $conn->query($sql);
+        $row = $query->fetch_assoc();
+        $grow_am = $row['growth'];
+        $grow_am = $grow_am + $g_amont;
+
+        $sql = "UPDATE wallet SET growth ='$grow_am' WHERE member_id='$memberid'";
+        $conn->query($sql);
+      }
+      else if (($datenow > $approvedate) and ($status == 'approved') and ($bool == 'false')) {
+        $sql = "UPDATE provide_help SET growth='true' WHERE id = '$id'";
+        $query = $conn->query($sql);
+
+        $g_amont = $amount * 2;
 
         $sql = "INSERT INTO growth(member_id,provide_amount,growth_amount) VALUES('$memberid','$amount','$g_amont')";
         $conn->query($sql);
