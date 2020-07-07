9844976ffff50f0426b43318f1f0f76e128deb91
Md Irshad <32608381+irshadmd@users.noreply.github.com>
2020-07-07 21:03:04 +0530

    user registration sms

--- bank/addmemberdata.php
+++ bank/addmemberdata.php
@@ -53,6 +53,30 @@
 					VALUES('$memberid','$name','$randno','$sponcer_name','$sponcer_id','$amount_p',NOW(),'$fir')";
 			$conn->query($sql);
 
+
+			$newphone = '91' . $phone;
+			// Authorisation details.
+			$username = "clubminibank@gmail.com";
+			$hash = "ed5bdeba90f28aa5d973bf3cb54863dd0b4ae39f3a6dd7e85087803376046499";
+
+			// Config variables. Consult http://api.textlocal.in/docs for more info.
+			$test = "0";
+
+			// Data for text message. This is the text message data.
+			$sender = "TXTLCL"; // This is who the message appears to be from.
+			$numbers = $newphone; // A single number or a comma-seperated list of numbers
+			$message = "This is a test message from the PHP API script.";
+			// 612 chars or less
+			// A single number or a comma-seperated list of numbers
+			$message = urlencode($message);
+			$data = "username=" . $username . "&hash=" . $hash . "&message=" . $message . "&sender=" . $sender . "&numbers=" . $numbers . "&test=" . $test;
+			$ch = curl_init('http://api.textlocal.in/send/?');
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$result = curl_exec($ch); // This is the result from the API
+			curl_close($ch);
+
 			$_SESSION['success'] = 'Member Added successfully'.'<br>'.'Member Id:'.$memberid.'<br>'.'Password:'.$password;
 		}
 		else{
