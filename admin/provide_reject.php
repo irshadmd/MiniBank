cd144499ff962541351958a3096cd08ba7d7b3f7
Md Irshad <32608381+irshadmd@users.noreply.github.com>
2020-07-11 12:46:52 +0530

    reject bug fixed

--- admin/provide_reject.php
+++ admin/provide_reject.php
@@ -5,8 +5,6 @@
 		$date = date('Y-m-d H:i:s');
 		$sql = "DELETE FROM provide_request WHERE provide_help_no = '$helpno'";
 		if($conn->query($sql)){
-			$sql = "UPDATE provide_help SET	status='rejected',approved_datetime='$date' ";
-			$conn->query($sql);
 			$_SESSION['success'] = 'Request rejected successfully';
 		}
 		else{
